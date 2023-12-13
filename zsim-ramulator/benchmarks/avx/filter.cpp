73ee457e78e36725f46e7ab046ea6d6f5c97f49c
midsterx <mimosk25@gmail.com>
2023-12-13 12:36:19 -0500

    feat: correct avx for filter

--- zsim-ramulator/benchmarks/avx/filter.cpp
+++ zsim-ramulator/benchmarks/avx/filter.cpp
@@ -13,38 +13,38 @@ const int ARRAY_SIZE = 32768;
 
 void filterRangeAVX2(float* input, float* output, int size, float lower_bound, float upper_bound) {
     zsim_roi_begin(); 
-    zsim_PIM_function_begin();
 
     __m256 lower_bound_vec = _mm256_set1_ps(lower_bound);
     __m256 upper_bound_vec = _mm256_set1_ps(upper_bound);
 
     for (int i = 0; i < size; i += 8) {
+        zsim_PIM_function_begin();
         __m256 data = _mm256_loadu_ps(&input[i]);
 
         __m256 mask = _mm256_and_ps(_mm256_cmp_ps(data, lower_bound_vec, _CMP_GE_OQ),
                                      _mm256_cmp_ps(data, upper_bound_vec, _CMP_LE_OQ));
 
         _mm256_maskstore_ps(&output[i], _mm256_castps_si256(mask), data);
+        zsim_PIM_function_end();
     }
 
-    zsim_PIM_function_end();
     zsim_roi_end();
 }
 
 void filterRange(float* input, float* output, int size, float lower_bound, float upper_bound) {
     zsim_roi_begin(); 
-    zsim_PIM_function_begin();
 
     for(int i = 0 ; i < size ; i++) {
+        zsim_PIM_function_begin();
         if(input[i]>=lower_bound && input[i]<=upper_bound) {
             output[i] = input[i];
         }
         else{
             output[i] = 0;
         }
+        zsim_PIM_function_end();
     }
 
-    zsim_PIM_function_end();
     zsim_roi_end();
 }
 
@@ -72,11 +72,11 @@ int main() {
     std::cout << ms_int.count() << "ms\n";
     std::cout << ms_double.count() << "ms\n";
 
-    // t1 = high_resolution_clock::now();
+    // auto t1 = high_resolution_clock::now();
     // filterRange(input, output, ARRAY_SIZE, lower_bound, upper_bound);
-    // t2 = high_resolution_clock::now();
-    // ms_int = duration_cast<milliseconds>(t2 - t1);
-    // ms_double = t2 - t1;
+    // auto t2 = high_resolution_clock::now();
+    // auto ms_int = duration_cast<milliseconds>(t2 - t1);
+    // duration<double, std::milli> ms_double = t2 - t1;
     // std::cout << ms_int.count() << "ms\n";
     // std::cout << ms_double.count() << "ms\n";
 
